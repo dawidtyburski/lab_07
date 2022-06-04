b0c02ab141069df899da992aeb803193f2664b6c
Dawid.Tyburski <Dawid.Tyburski@PK5-03>
2022-06-04 14:28:05 +0200

    Dodaj pliki projektów.

---
+++ lab_07/Models/Persons.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Models
+{
+    public class Persons
+    {
+       public int PersonID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ lab_07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace lab_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProjectDBContext db = new ProjectDBContext();
+            var per = from p in db.Persons
+                        select p;
+            foreach(var item in per)
+            {
+                Console.WriteLine($"{item.Name} {item.Surname}");
+            }
+        }
+    }
+}
