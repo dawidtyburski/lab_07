a39c4155402ac85681c82ad51db727e62a47a6f0
Dawid Tyburski <Dawid.Tyburski@microsoft.wsei.edu.pl>
2022-06-17 23:29:51 +0200

    a

---
+++ lab_07/BloggingContext.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<User> User { get; set; }
+    public DbSet<Role> Role { get; set; }
+
+    public string ConnectionString { get; }
+
+    public BloggingContext(string connectionString)
+    {
+        this.ConnectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(this.ConnectionString);
+    }
+}
+
+public class User
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public long RoleId { get; set; }
+    public Role Role { get; set; }
+
+
+}
+
+public class Role
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+
+    
+}
--- lab_07/Program.cs
+++ lab_07/Program.cs
@@ -1,22 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
-
-namespace lab_07
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
+        string connectionString = (@"server=(localdb)\mssqllocaldb;database=lab07db;trusted_connection=true;");
 
-            ProjectDBContext db = new ProjectDBContext();
-            var per = from p in db.Persons
-                        select p;
-            foreach(var item in per)
+        using (BloggingContext db = new BloggingContext(connectionString))
+        {
+            Console.WriteLine($"Database ConnectionString: {db.ConnectionString}.");
+
+            while(connectionString != null)
             {
-                Console.WriteLine($"{item.Name} {item.Surname}");
-            }
+                Console.WriteLine("Wybor opcji");
+                Console.WriteLine("1.Dodaj uzytkownika");
+                Console.WriteLine("2.Pokaz uzytkownikow");
+                string wybor = Console.ReadLine();
+
+                if(wybor == "1")
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine("--Dodawanie uzytkownika...");
+                    Console.WriteLine("Podaj imie");
+                    string imie = Console.ReadLine();
+                    Console.WriteLine("Podaj nazwisko");
+                    string nazwisko = Console.ReadLine();
+                    Console.WriteLine("Podaj ID funkcji");
+                    var roles = from r in db.Role
+                                 select r;
+                    foreach (var r in roles)
+                    {
+                        
+                        Console.WriteLine($"{r.Id}. {r.Title}");
+                        
+                    }
+                    int funkcja = Int32.Parse(Console.ReadLine());
+                    var role = from ro in db.Role
+                               where ro.Id == funkcja
+                               select ro;
+
+                   foreach (var ro in role)
+                   {
+                        db.Add(new User { Name = imie, Surname = nazwisko, Role = ro });
+                   }
+                   
+                    db.SaveChanges();
+
+                    Console.WriteLine("...");
+                    Console.WriteLine("Uzytkownik zostal dodany...");
+                    Console.WriteLine("\n");
+                }
+                else if(wybor == "2")
+                {
+                    Console.WriteLine("--Lista uzytkownikow...");
+
+                    var users = from u in db.User
+                                    //orderby u.Id
+                                select u;
+                    foreach (var u in users)
+                        {
+                                Console.WriteLine($"{u.Id}.{u.Name} {u.Surname} [{u.Role.Title}]");
+
+                        }
+                        Console.WriteLine("\n");                    
+                }                                                    
+                else
+                {
+                    Console.WriteLine("Bledny wybor");
+                }
+            }                      
         }
     }
 }
